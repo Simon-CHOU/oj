75d0dd1c4ab55686c42a8d787c8871390cb82ef3
Simon-Chou <mrsimonzhou@gmail.com>
2019-06-01 16:41:49 +0800

    在Windows下调试通过规范、静态、查重生成的文件

---
+++ plugins/duplicat-checker/duplicat-checker-testcases/recheckHtml/js/array.js
@@ -0,0 +1,2 @@
+var errorType = ['查重通过','查重未通过']
+var errorMap = [{value:0, name:'查重通过'},{value:8, name:'查重未通过'}]
---
+++ plugins/style-checker/style-checker-testcases/cpplintHtml/js/array.js
@@ -0,0 +1,3 @@
+var errorType = ['函数注释错误','函数命名错误',]
+var errorNum = [11,1,]
+var errorMap = [{value:11, name:'函数注释错误'},{value:1, name:'函数命名错误'},]
