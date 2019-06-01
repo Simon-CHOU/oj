71a5191191d3b934d8895018c99e2ff44dbadd64
Simon-Chou <mrsimonzhou@gmail.com>
2019-06-02 07:18:32 +0800

    重新生成了一次批量风格检查

--- plugins/style-checker/style-checker-testcases/cpplintHtml/js/array.js
+++ plugins/style-checker/style-checker-testcases/cpplintHtml/js/array.js
@@ -1,3 +1,3 @@
-var errorType = []
-var errorNum = []
-var errorMap = []
+var errorType = ['函数注释错误','函数命名错误',]
+var errorNum = [13,1,]
+var errorMap = [{value:13, name:'函数注释错误'},{value:1, name:'函数命名错误'},]
